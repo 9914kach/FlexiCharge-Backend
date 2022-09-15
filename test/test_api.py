14b40e5d4b2ccb62146e45ffaffcdefc14d75bf6
9914kach <9914kach@gmail.com>
2022-09-15 10:51:29 +0200

    Testing the actions

--- test/test_api.py
+++ test/test_api.py
@@ -5,7 +5,7 @@ class TestApi:
     
     def test_api_status_code(self):
         r = requests.get("http://18.202.253.30:8080/")
-        assert r.status_code == 200, "Status code is not 200"
+        assert r.status_code ==300, "Status code is not 200"
         
     def test_api_encoding(self):
         r = requests.get("http://18.202.253.30:8080/")
