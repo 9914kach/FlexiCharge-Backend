ede7449bcaee7196a7d34c54680ffee836c48346
9914kach <9914kach@gmail.com>
2022-09-26 22:36:51 +0200

    Transaction Api tests

--- example.py
+++ example.py
@@ -1,17 +1,15 @@
 import requests
 import pytest
     
-def test():
-    chargerid= "100011"
-    url = "http://18.202.253.30:8080/chargers/" + chargerid
-        
+def test(userid = "1"):
+    url = "http://18.202.253.30:8080/transactions//userTransactions/" + userid
+
+
     r = requests.get(url)
  
     response = r.json()
     
-    foo = response["status"]
-
-    print(foo)
-
-    assert foo == "Available", "Charger is not available"
+    
+    for i in response:
+        print(i["transactionID"])
 test()
