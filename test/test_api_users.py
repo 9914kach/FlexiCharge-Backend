73ee91685323f6e77900b62a8d5e95f69cf930ca
9914kach <9914kach@gmail.com>
2022-09-28 10:54:23 +0200

    Added forgot passwords test to user tests

--- test/test_api_users.py
+++ test/test_api_users.py
@@ -50,3 +50,31 @@ class TestApiUsers:
                 #Assert
                 assert request.status_code == 200, "Status code is not 200"
                 
+                
+        def test_forgot_password(self, username = "9914kach@gmail.com"):
+                
+                #Arrange
+                url = "http://18.202.253.30:8080/auth/forgot-password/" + username
+                
+                #Act
+                request = requests.post(url)
+                
+                #Assert
+                assert request.status_code == 200, "Status code is not 200"
+                
+        def test_confirm_forgot_password(self):
+                
+                #Arrange
+                
+                url = "http://18.202.253.30:8080/auth/confirm-forgot-password"
+                payload = {
+                        "username": "9914kach@gmail.com",
+                        "password": "Password123?",
+                        "confirmationCode": "856404"
+                }
+                
+                #Act
+                request = requests.post(url, json=payload)
+                
+                #Assert
+                assert request.status_code == 200, "Status code is not 200"
